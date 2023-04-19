0ac11dd9abadb95af01f725e5b16ecea35af8ea6
nemeclogan <logannemel@gmail.com>
2023-04-19 17:17:28 -0400

    added apple asset

--- starter_game/scene.js
+++ starter_game/scene.js
@@ -5,6 +5,7 @@ class Scene {
     this.load.image('ground', 'assets/platform.png');
     this.load.image('star', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
+    this.load.image('apple', 'assets/apple.png');
     this.load.audio('BGM', 'assets/BGM.mp3');
     this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
     this.load.spritesheet('player1', 'assets/Player1Cat.png', { frameWidth: 12, frameHeight: 12 });
